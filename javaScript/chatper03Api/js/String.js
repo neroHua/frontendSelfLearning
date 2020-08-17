975cae322825cdaf5796e58c0ee8533abc5b140f
neroHua <2676337735@qq.com>
2020-08-17 09:00:10 +0800

    add string

---
+++ javaScript/chatper03Api/js/String.js
@@ -0,0 +1,20 @@
+var a = "Hello";
+document.write(a + "<br/>");
+
+a = a.concat(" World");
+document.writeln(a + "<br/>");
+
+var b = a.substring(1, 5);
+document.writeln(b + "<br/>");
+
+var c = a.charAt(0);
+document.writeln(c + "<br/>");
+
+var d = a.toLowerCase();
+document.writeln(d + "<br/>");
+
+var e = a.toUpperCase();
+document.writeln(e + "<br/>");
+
+var f = a.split('l')
+document.writeln(f + "<br/>");
