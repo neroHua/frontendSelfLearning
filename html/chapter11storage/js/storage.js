ebffcc4be3adf565912afc7fa30dcaf41ef5cb7b
neroHua <2676337735@qq.com>
2020-08-18 17:20:18 +0800

    add storage

---
+++ html/chapter11storage/js/storage.js
@@ -0,0 +1,7 @@
+alert(localStorage.getItem("key1"));
+localStorage.setItem("key1", "value1")
+alert(localStorage.getItem("key1"));
+
+alert(sessionStorage.getItem("key2"));
+sessionStorage.setItem("key2", "value2")
+alert(sessionStorage.getItem("key2"));
